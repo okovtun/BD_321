312cae87c27c43487247f5e00dafc2a02f9d3af1
Clayman <Clayman@DESKTOP-DM8E77U>
2023-02-21 19:00:16 +0300

    DataTypes day 1

---
+++ DataTypes/DataTypes/main.cpp
@@ -0,0 +1,14 @@
+//DataTypes
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "DataTypes" << endl;
+	//оба этих выражения приводят к одному и тому же результату
+	cout << "\n";	//newline
+	cout << endl;	//endline
+	cout << true << endl;
+	cout << false << endl;
+}
---
+++ Hardware/ReadMe.txt
@@ -0,0 +1,4 @@
+https://www.youtube.com/watch?v=1OC_9iEGJ24&list=PLeqyOOqxeiIMr5G8nf1P8iQwzzwZIv1vW
+
+TODO:
+Выучить теорию!
