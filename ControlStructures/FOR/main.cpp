8b041c890aa57c8527d5658783ec7702374e23e5
Clayman <Clayman@DESKTOP-DM8E77U>
2023-04-04 16:38:15 +0300

    NestedFOR day 2

--- ControlStructures/FOR/main.cpp
+++ ControlStructures/FOR/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<ctime>
 #include<Windows.h>
 using namespace std;
 using std::cin;
@@ -9,7 +10,11 @@ using std::endl;
 //#define FACTORIAL
 //#define POWER
 //#define ASCII
+//#define MULTIPLICATION_TABLE
 //#define CLOCK
+//#define PYTHAGORAS_TABLE
+//#define FIBONACCI_1
+//#define FIBONACCI_2
 
 void main()
 {
@@ -65,15 +70,24 @@ void main()
 	}
 #endif // ASCII
 
+#ifdef MULTIPLICATION_TABLE
 	int n = 10;
 	for (int i = 1; i <= n; i++)
 	{
 		for (int j = 1; j <= n; j++)
 		{
-			cout << i << " * " << j << " = " << i*j << endl;
+			/*if (i < 10)cout << " ";
+			cout << i << " * ";
+			if (j < 10)cout << " ";
+			cout << j << " = ";
+			if (i*j < 100)cout << " ";
+			if (i*j < 10)cout << " ";
+			cout << i * j << endl;*/
+			cout << i * j << "\t";
 		}
 		cout << endl;
 	}
+#endif // MULTIPLICATION_TABLE
 
 #ifdef CLOCK
 	for (int hh = 0; hh < 24; hh++)
@@ -90,4 +104,61 @@ void main()
 	}
 #endif // CLOCK
 
+#ifdef PYTHAGORAS_TABLE
+	for (int i = 1; i <= 10; i++)
+	{
+		for (int j = 1; j <= 10; j++)
+		{
+			cout.width(5);
+			//cout << left;
+			cout << i * j;
+		}
+		cout << endl;
+	}
+#endif // PYTHAGORAS_TABLE
+
+#ifdef FIBONACCI_1
+	int n;
+	cout << "¬ведите предельное число: "; cin >> n;
+	for (int a = 0, b = 1, c = a + b; a < n; a = b, b = c, c = a + b)
+	{
+		cout << a << "\t";
+	}
+	cout << endl;
+#endif // FIBONACCI_1
+
+#ifdef FIBONACCI_2
+	int n;
+	cout << "¬ведите количество членов р€да ‘ибоначчи: "; cin >> n;
+	for (unsigned long long int i = 0, a = 0, b = 1, c = a + b; i++ < n; c = (a = b) + (b = c))
+	{
+		cout << i << "\t" << a << "\n";
+	}
+	cout << endl;
+#endif // FIBONACCI_2
+
+	int n;
+	cout << "¬ведите предельное число: "; cin >> n;
+	time_t t_start = time(NULL);
+	for (int i = 0; i < n; i++)
+	{
+		bool simple = true;	//предположим что число простое,
+		//но это нужно проветрить:
+		for (int j = 2; j <= sqrt(i); j++)
+		{
+			if (i % j == 0)
+			{
+				simple = false;
+				break;	//если условие уже выполнилось, значит мы уже вы€снили,
+				//что число составное, а это значит что дальше искать нет смысла,
+				//мы уже нашли все что нужно.
+				// лючевое слово 'break' прерывает текущую итерацию, и все последующие
+				//break; //прервет только j-й for, на i-й for, оно никак не подействует
+			}
+		}
+		if (simple)cout << i << "\t";
+	}
+	time_t t_end = time(NULL);
+	cout << endl;
+	cout << "ѕоиск зан€л " << t_end - t_start << " секунд\n";
 }
--- ControlStructures/HomeWork/main.cpp
+++ ControlStructures/HomeWork/main.cpp
@@ -1,53 +1,8 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-#define delimiter "----------------------------------------------------------------------------------------\n"
-#define FACTORIAL
-#define POWER
-
-void ascii()
-{
-	cout << "ASCII" << endl;
-	for (int i = 0; i <= 255; i++)
-	{
-		cout << (char)i;
-		if ((i + 1) % 16 == 0)
-		{
-			cout << endl;
-		}
-	}
-	cout << delimiter;
-}
 
 void main()
 {
-	ascii();
 	setlocale(LC_ALL, "");
-	ascii();						//после setlocale некоторые символы мен€ютс€ на другие или на вопросики
-#ifdef FACTORIAL
-	cout << "Factorial" << endl;
-	int num;
-	int factorial = 1;
-	cout << "¬ведите число: "; cin >> num;
-	for (int i = 1; i <= num; i++)
-	{
-		factorial *= i;
-	}
-	cout << num << "! = " << factorial << endl;
-	cout << delimiter;
-#endif
-#ifdef POWER
-	cout << "Power" << endl;
-	int a;
-	int n;
-	cout << "¬ведите основание степени: "; cin >> a;
-	cout << "¬ какую степень возводить: "; cin >> n;
-	int power = 1;
-	for (int i = 1; i <= n; i++)
-	{
-		power *= a;
-	}
-	cout << a << "^" << n << " = " << power << endl;
-	cout << delimiter;
-#endif
+
 }
