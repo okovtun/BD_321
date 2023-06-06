62efc52206d15fca240111ca527b45732bd59b74
Clayman <Clayman@DESKTOP-DM8E77U>
2023-06-06 16:25:53 +0300

    Functions

---
+++ Functions/Arrays/main.cpp
@@ -0,0 +1,63 @@
+﻿#include<iostream>
+using namespace std;
+
+#define tab "\t"
+#define delimiter "\n----------------------------------\n"
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+void Sort(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	
+	FillRand(arr, n);
+	Print(arr, n);
+	Sort(arr, n);
+	Print(arr, n);
+
+	const int SIZE = 8;
+	int brr[SIZE];
+
+	FillRand(brr, SIZE);
+	Print(brr, SIZE);
+	Sort(brr, SIZE);
+	Print(brr, SIZE);
+}
+
+void FillRand(int arr[], const int n)
+{
+	//Заполнение массива случайными числами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Sort(int arr[], const int n)
+{
+	//Сортировка массива:
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
--- Functions/Functions/main.cpp
+++ Functions/Functions/main.cpp
@@ -1,8 +1,44 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
+
+int Add(int a = 0, int b = 0, int c = 0);	//Прототип функции (Объявление функции - Function declaration)
+int Sub(int a, int b);
+int Mul(int a, int b);
+double Div(int a, int b);
+
+//Default arguments, Default parameters
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	cout << "Hello Functions" << endl;
+	int a, b;
+	cout << "Введите два числа: "; cin >> a >> b;
+	int c = Add(a,b);	//Вызов (использование) функции (Function call)
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Sub(a, b) << endl;
+	cout << a << " * " << b << " = " << Mul(a, b) << endl;
+	cout << a << " / " << b << " = " << Div(a, b) << endl;
+	//cout << a << " ^ " << b << " = " << power(a, b) << endl;
+}
+
+int Add(int a = 0, int b = 0, int c = 0)		//Реализация функции (Определение функции - Function definition)
+{
+	//Addition - сложение
+	int sum = a + b + c;
+	return sum;
+}
+int Sub(int a, int b)
+{
+	//Subtraction - вычитание
+	return a - b;
+}
+int Mul(int a, int b)
+{
+	//Multiplication - умножение
+	return a * b;
+}
+double Div(int a, int b)
+{
+	//Division - деление
+	return (double)a / b;
 }
